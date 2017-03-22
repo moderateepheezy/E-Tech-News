9e2715262f4b41af5cf0d386a506df6cf3c002b8
Afees <moderateepheezy@gmail.com>
2017-03-22 19:49:45 +0100

    Add Intro Screen

---
+++ app/src/main/java/com/simpumind/e_tech_news/IntroActivity.java
@@ -0,0 +1,13 @@
+package com.simpumind.e_tech_news;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
